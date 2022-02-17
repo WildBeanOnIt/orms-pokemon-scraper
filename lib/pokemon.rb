e5e0186f4801347576364a864532cc8cbaf9aef1
Julio Aguilera Jr <julioaguilera337@gmail.com>
2022-02-16 19:45:15 -0600

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,23 @@
 class Pokemon
+
+    attr_accessor :name, :type, :db
+    attr_reader :id
+
+    def initialize(id:, name:, type:, db:)
+        @id = id
+        @name = name
+        @type = type
+        @db = db
+    end
+
+
+    def self.save
+        sql = <<-SQL
+            INSERT INTO pokemon (name, type, id) VALUES (?, ?, ?)
+        SQL
+    
+
+        DB[:conn].execute(sql, self.name, self.type, self.id)
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+    end
 end
